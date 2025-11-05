4c8d17a63edd1c0202843baf342723d096e9468c
Spectre-punk <135066306+Spectre-punk@users.noreply.github.com>
2025-11-05 21:21:52 +0000

    estructura principal

---
+++ main.java
@@ -0,0 +1,13 @@
+
+public class Main {
+
+    public static void main(String[] args) {
+        while (true) {
+            System.out.println("Opciones:");
+            System.out.println("1. Opción 1");
+
+            
+        }
+
+    }
+}
